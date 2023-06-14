8a737b3ed21042bed981bf064c4f06b185296110
Maksim Lisovoi <lisovoi123@gmail.com>
2023-06-14 16:31:17 +0300

    - exhaustive deps eslint

--- src/components/AllProductsSection/AllProductsSection.jsx
+++ src/components/AllProductsSection/AllProductsSection.jsx
@@ -30,7 +30,6 @@ export const AllProductsSection = () => {
     setFilter(name);
 
     fetchProductsByCategory(name);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
